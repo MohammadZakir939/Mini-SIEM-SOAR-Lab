aea132507d7ea87135bca1d3e786384f6263d730
Shaik Mohammad Zakir <189457322+MohammadZakir939@users.noreply.github.com>
2026-06-23 20:13:44 +0530

    Add dashboard_summary.py

---
+++ dashboard_summary.py
@@ -0,0 +1,13 @@
+total_incidents = 12
+critical_incidents = 4
+open_cases = 3
+mitre_coverage = 4
+
+print("\n===== SOC DASHBOARD =====\n")
+
+print(f"Total Incidents: {total_incidents}")
+print(f"Critical Incidents: {critical_incidents}")
+print(f"Open Cases: {open_cases}")
+print(f"MITRE Techniques Covered: {mitre_coverage}")
+
+print("\n=========================\n")
